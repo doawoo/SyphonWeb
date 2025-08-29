804d4a684abb7c5dfa4f1b236283673da7119e96
Digit Dog <woof@puppy.surf>
2025-08-29 09:48:14 -0700

    Remove utility window, add Database driven bookmark list to main view

--- Sources/SyphonWeb/main.swift
+++ Sources/SyphonWeb/main.swift
@@ -3,13 +3,20 @@ import MetalKit
 import SwiftUI
 import Syphon
 
+// Stop app from napping
 var activity: NSObjectProtocol?
-activity = ProcessInfo().beginActivity(options: ProcessInfo.ActivityOptions.userInitiated, reason: "No Napping!")
+activity = ProcessInfo().beginActivity(
+  options: ProcessInfo.ActivityOptions.userInitiated, reason: "No Napping!")
 
+// Init metal, syphon and SQLite
+NSLog("Creating Metal device and Syphon server...")
 let viewWidth = 1280.0
 let viewHeight = 720.0
 let metalDevice: MTLDevice = MTLCreateSystemDefaultDevice()!
 let server: SyphonMetalServer = SyphonMetalServer.init(name: "SyphonWeb", device: metalDevice)
+
+NSLog("Opening SQLite database connection...")
+nonisolated(unsafe) let databaseConn = initDatabase()
 
 // AppKit Stuff
 class WindowDelegate: NSObject, NSWindowDelegate {
@@ -19,28 +26,29 @@ class WindowDelegate: NSObject, NSWindowDelegate {
   }
 }
 
+@available(macOS 14, *)
 @MainActor
 class AppDelegate: NSObject, NSApplicationDelegate {
   let mainWindow: NSWindow = NSWindow()
-  let utilityWindow: NSWindow = NSWindow()
-
   let mainWindowDelegate: WindowDelegate = WindowDelegate()
-  let utilityWindowDelegate: WindowDelegate = WindowDelegate()
 
   func applicationDidFinishLaunching(_ notification: Notification) {
 
     // Main Window
     let mainSize: CGSize = CGSize(
-      width: viewWidth, height: viewHeight)
+      width: viewWidth + 200, height: viewHeight)
     mainWindow.setContentSize(mainSize)
-    mainWindow.styleMask = [.closable, .miniaturizable, .titled]
+    mainWindow.styleMask = [.closable, .titled]
     mainWindow.delegate = mainWindowDelegate
     mainWindow.title = "SyphonWeb"
 
     // Create state object and init Metal objects
     let state: WebViewState = WebViewState()
     state.frameServer = server
-    state.initMetal(width: viewWidth / mainWindow.backingScaleFactor, height: viewHeight / mainWindow.backingScaleFactor, scaleFactor: mainWindow.backingScaleFactor)
+    state.initMetal(
+      width: viewWidth / mainWindow.backingScaleFactor,
+      height: viewHeight / mainWindow.backingScaleFactor, scaleFactor: mainWindow.backingScaleFactor
+    )
 
     let mainViewInst = MainView(state: state)
     let mainView: NSHostingView<MainView> = NSHostingView(rootView: mainViewInst)
@@ -50,32 +58,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     mainWindow.center()
     mainWindow.makeKeyAndOrderFront(mainWindow)
 
-    // Utility Window
-    let utilitySize: CGSize = CGSize(
-      width: 800 / mainWindow.backingScaleFactor, height: 300 / mainWindow.backingScaleFactor)
-    utilityWindow.setContentSize(utilitySize)
-    utilityWindow.styleMask = [.titled]
-    utilityWindow.delegate = utilityWindowDelegate
-    utilityWindow.title = "Settings"
-
-    let utilityViewInst = UtilityView(onNavigate: { url in
-      mainViewInst.navigateTo(urlString: url)
-    })
-    
-    let utilityView: NSHostingView<UtilityView> = NSHostingView(rootView: utilityViewInst)
-    utilityView.frame = CGRect(
-      origin: .zero,
-      size: utilitySize)
-    utilityWindow.contentView!.addSubview(utilityView)
-    utilityWindow.makeKeyAndOrderFront(utilityWindow)
-
     NSApp.setActivationPolicy(.regular)
     NSApp.activate(ignoringOtherApps: true)
   }
 }
 
 // Start!
-let app: NSApplication = NSApplication.shared
-let delegate: AppDelegate = AppDelegate()
-app.delegate = delegate
-app.run()
+if #available(macOS 14, *) {
+  let app: NSApplication = NSApplication.shared
+  let delegate: AppDelegate = AppDelegate()
+  // Fallback on earlier versions
+  app.delegate = delegate
+  app.run()
+
+} else {
+  NSLog("You cannot run this app on this version of macOS!")
+}
--- Sources/SyphonWeb/mainView.swift
+++ Sources/SyphonWeb/mainView.swift
@@ -1,13 +1,52 @@
 import SwiftUI
 
+@available(macOS 14, *)
 struct MainView: View {
   @ObservedObject var state: WebViewState
+  @State private var bookmarks: [Bookmark] = Bookmark.getAll()
+  @State private var selectedBookmark: Bookmark?
 
   var body: some View {
-    GeometryReader { geometry in
+    HSplitView {
+      List(selection: $selectedBookmark) {
+        // Filter favs and non-faves
+        let nonFavorites = bookmarks.filter { mrk in
+          !mrk.favorite
+        }
+
+        let favorites = bookmarks.filter { mrk in
+          mrk.favorite
+        }
+
+        Section("Favorites") {
+          ForEach(
+            favorites
+          ) { bookmark in
+            makeBookmark(bookmark: bookmark)
+          }
+        }
+        Section("Bookmarks") {
+          ForEach(
+            nonFavorites
+          ) { bookmark in
+            makeBookmark(bookmark: bookmark)
+          }
+        }
+      }.listStyle(.sidebar)
+        .safeAreaInset(edge: .bottom) {
+          VStack {
+            Button(action: {
+              // TODO: Add bookmark to database
+            }) {
+              Label("Add", systemImage: "plus")
+            }
+          }.padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }.frame(width: 200, alignment: .top)
       WebView(state: state).frame(
-        minWidth: geometry.size.width, maxWidth: geometry.size.width,
-        minHeight: geometry.size.height, maxHeight: geometry.size.height)
+        minWidth: viewWidth, maxWidth: viewWidth,
+        minHeight: viewHeight, maxHeight: viewHeight,
+      ).scaleEffect(0.90)
     }
   }
 
@@ -18,8 +57,35 @@ struct MainView: View {
           urlToNavigate = httpsURL
         }
       }
-      
+
       state.url = urlToNavigate
     }
+  }
+
+  func refreshBookmarks() {
+    bookmarks = []
+    bookmarks = Bookmark.getAll()
+  }
+
+  @ViewBuilder func makeBookmark(bookmark: Bookmark) -> some View {
+    let systemImageName =
+      if bookmark.favorite {
+        "star.fill"
+      } else {
+        "star"
+      }
+
+    HStack {
+      Label("Favorite", systemImage: systemImageName).labelStyle(.iconOnly).onTapGesture {
+        bookmark.toggleFavorite()
+        refreshBookmarks()
+      }
+      Label(bookmark.name, systemImage: "link")
+
+    }.onTapGesture {
+      selectedBookmark = bookmark
+      navigateTo(urlString: bookmark.url)
+    }.tag(bookmark)
+
   }
 }
